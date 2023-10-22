bdb5fe01bee8a75095d5b77b930742ea42c553d7
lEVSH@ <helliry@yandex.ru>
2023-10-22 12:53:49 +0300

    dz2 Hello, world!
    Выполнить задание, описанное в 02.homework.pdf

---
+++ move_me_out.cpp
@@ -0,0 +1,11 @@
+//
+// Created by helli on 22.10.2023.
+//
+
+#include "move_me_out.h"
+
+#include <iostream>
+
+void todo_move_me_out() {
+    std::cout << "This function should be located in the separated source file" << std::endl;
+}
